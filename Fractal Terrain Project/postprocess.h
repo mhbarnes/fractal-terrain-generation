815608824f2320c51bea33853131c02ee287c6cf
Michael Barnes <barnemic@oregonstate.edu>
2018-12-16 02:28:52 -0800

    added boundary case functions for gauss_blur()

--- Fractal Terrain Project/postprocess.h
+++ Fractal Terrain Project/postprocess.h
@@ -7,5 +7,14 @@
 bitmap_image sharpen(&bitmap_image);
 bitmap_image box_blur(&bitmap_image);
 bitmap_image gauss_blur(&bitmap_image);
+//gauss_blur() helper functions
+void top_left(RGB [][3], &bitmap_image);
+void top_right(RGB[][3], &bitmap_image);
+void bot_left(RGB[][3], &bitmap_image);
+void bot_right(RGB[][3], &bitmap_image);
+void top_edge(RGB[][3], &bitmap_image);
+void left_edge(RGB[][3], &bitmap_image);
+void right_edge(RGB[][3], &bitmap_image);
+void bot_edge(RGB[][3], &bitmap_image);
 
 #endif
